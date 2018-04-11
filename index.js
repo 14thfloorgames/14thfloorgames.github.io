cfd9060aeb1636316a913620142f96a2eb4600aa
corey2915 <32797309+corey2915@users.noreply.github.com>
2018-04-11 18:04:20 -0400

    start 1

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-phrase = "grizzly bear/cub/car/blacl/toy/ty/code/hi/no/tame/frame/land/fill/landfill";
-borad =["corey","mcorlky","bear","door","kids","school","codnext","DC"]
-phrasefinder_url = 'https://cors-anywhere.herokuapp.com/http://phrasefinder.io/search?corpus=eng-us&topk=10&query=' + encodeURIComponent(phrase);
-console.log(phrasefinder_url)
-$.ajax({
-    type: "GET",
-    url: phrasefinder_url,
-    dataType: 'json',
-    async: false,
-    success: function(phrasefinder_response) {
-        for (x in phrasefinder_response.phrases) {
-            console.log(phrasefinder_response.phrases[x].mc);
-            console.log(phrasefinder_response.phrases[x].tks[0].tt);
-            console.log(phrasefinder_response.phrases[x].tks[1].tt);
-        }
 
-        //console.log(phrasefinder_response.phrases);
-
-    },
-    error: function(requtes, error, status) {
-        console.log(["status", status]);
-    }
-});
-
+borad =["corey","mcorlky","bear","door","kids","school","codnext","DC","cub","brown","white"]
 get_count = function(codename, words, topk) {
     query = codename + ' ';
-    console.log(query)
+    //console.log(query)
     wordCount = words.length
-    console.log(wordCount)
+    //console.log(wordCount)
     for (word in words) {
-        console.log(words[word])
+        //console.log(words[word])
         wordCount --
         query += words[word]
         if(wordCount!==0){
@@ -36,11 +14,37 @@ get_count = function(codename, words, topk) {
            query += '/'
         }
 
-        console.log(query)
-        console.log(wordCount)
+        // console.log(query)
+        // console.log(wordCount)
     }
   phrasefinder_url = 'https://cors-anywhere.herokuapp.com/http://phrasefinder.io/search?corpus=eng-us&topk=10&query=' + encodeURIComponent(query);
   console.log(phrasefinder_url)
+  counts = {}
+
+function success(phrasefinder_response) {
+  for (x in phrasefinder_response.phrases) {
+    count = phrasefinder_response.phrases[x].mc
+    console.log(count)
+    word = phrasefinder_response.phrases[x].tks[1].tt.toLowerCase()
+    console.log(word)
+    if(counts[word]===undefined){
+      counts[word] = count
+    }else{
+      counts[word] += count
+    }
+console.log(counts)
+  }
+}
+  $.ajax({
+      type: "GET",
+      url: phrasefinder_url,
+      dataType: 'json',
+      async: false,
+      success:success,
+      error: function(requtes, error, status) {
+          console.log(["status", status]);
+      }
+  });
 }
 
 get_count('grizzly', borad, 'car');
