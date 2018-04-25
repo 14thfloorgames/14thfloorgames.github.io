66800641d4e5231c773120320853a4c0bd934aac
corey2915 <32797309+corey2915@users.noreply.github.com>
2018-04-25 17:46:26 -0400

    done mapping

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 
-borad =["corey","mcorlky","bear","door","kids","school","codnext","DC","cub","brown","white"]
+borad =["corey","mcorlky","bear","door","kids","school","codnext","DC","cub","brown","white","copper","explain","ill-fated","truck",
+"neat"
+,"unite"
+,"branch"
+,"educated"
+,"tenuous"
+,"hum"
+,"decisive"
+,"notice"]
 get_count = function(codename, words, topk) {
     query = codename + ' ';
     //console.log(query)
@@ -19,18 +27,23 @@ get_count = function(codename, words, topk) {
     }
   phrasefinder_url = 'https://cors-anywhere.herokuapp.com/http://phrasefinder.io/search?corpus=eng-us&topk=10&query=' + encodeURIComponent(query);
   console.log(phrasefinder_url)
-  counts = {}
-
+  //counts = []
+  counts = new Map();
 function success(phrasefinder_response) {
   for (x in phrasefinder_response.phrases) {
     count = phrasefinder_response.phrases[x].mc
     console.log(count)
     word = phrasefinder_response.phrases[x].tks[1].tt.toLowerCase()
     console.log(word)
-    if(counts[word]===undefined){
-      counts[word] = count
+      console.log(counts.size)
+    if(counts.get(word)===undefined){
+      counts.set(word,count)
+      if(counts.size == topk){
+        console.log("break")
+        break//stop
+      }
     }else{
-      counts[word] += count
+      counts.set(word, counts.get(word) + count)
     }
 console.log(counts)
   }
@@ -45,6 +58,8 @@ console.log(counts)
           console.log(["status", status]);
       }
   });
+return counts.keys()
 }
 
-get_count('grizzly', borad, 'car');
+console.log(get_count('dog', borad, 6))
+//get_count('dog', borad, 6);
